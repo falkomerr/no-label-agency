506a04b1c4e53e67e5270dbff5bccdcf5b9fdc7d
Rodoro <rodoro.game@gmail.com>
2024-06-22 00:16:50 +0300

    Fix range slider

--- src/components/interfaces/RangeSlider.tsx
+++ src/components/interfaces/RangeSlider.tsx
@@ -32,6 +32,15 @@ const RangeSlider: React.FC<Props> = ({ value, setValue, min, max }) => {
             onChange={handleChange}
             sx={{
                 color: "#fff",
+                '& .MuiSlider-valueLabel': {
+                    display: "none",
+                    '&::before': {
+                        display: 'none',
+                    },
+                    '& *': {
+                        display: "none",
+                    },
+                },
                 '& .MuiSlider-rail': { opacity: 0.2, height: "2px" },
                 '& .MuiSlider-track': { opacity: 0 },
                 '& .MuiSlider-thumb': { opacity: 0.8, background: "#FF4D00", '&:before': { background: "white", height: "8px", width: "8px", opacity: 1, boxShadow: '0 0 23px 0 rgba(255, 77, 0, 0.6), 0 0 29px 0 rgba(255, 77, 0, 0.3)' }, '&:focus, &:hover, &.Mui-active': { boxShadow: '0 0 23px 0 rgba(255, 77, 0, 0.6), 0 0 29px 0 rgba(255, 77, 0, 0.3)' } },
